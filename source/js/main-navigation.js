b0ffb91831c3796b9009848fe77855d47fd8f733
Anastasia Kuznetsova <kuznetsovanastasija@gmail.com>
2020-12-16 20:46:19 +0400

    Главная страница

--- source/js/main-navigation.js
+++ source/js/main-navigation.js
@@ -1,10 +1,12 @@
 const openButton = document.querySelector('.main-nav__toggle');
 const closeButton = document.querySelector('.main-nav__close');
 const mainNav = document.querySelector('.main-nav__list');
-const navClosed = document.querySelector('.main-nav__list--closed');
 
+openButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mainNav.classList.remove(".main-nav__list--close");
+});
 closeButton.addEventListener("click", function (evt) {
   evt.preventDefault();
-  mainNav.classList.add('.main-nav__close');
-  mainNav.classList.remove(".main-nav__list");
+  mainNav.classList.add('.main-nav__list--close');
 });
